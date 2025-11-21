b0a0ad9140d96566fa6c172a4d660b47227ac6fc
MateoF024 <mateoff24@gmail.com>
2025-11-21 12:10:01 -0300

    first commit

---
+++ src/client/java/com/mateof24/OnTimeClient.java
@@ -0,0 +1,9 @@
+package com.mateof24;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OnTimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
